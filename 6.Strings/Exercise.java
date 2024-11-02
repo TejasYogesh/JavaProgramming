2aef35fb2f15dd6bac16158f1db5759471d2cccf
Tejas Yogesh <yogeshtejaslalitha@gmail.com>
2024-11-02 06:45:32 +0530

    Strings

---
+++ 6.Strings/Exercise.java
@@ -0,0 +1,9 @@
+public class Exercise {
+    public static void main(String [] args){
+        String str1 = "JavaProgram";
+        System.out.println(str1);
+        String str2 = new String("Java Programming");
+        System.out.println(str2);
+        System.out.println("Hello world");
+    }
+}
